3ac86d2b4ef1b671027b41218a60d20ffc81be25
Paritosh Panda <paritoshpanda21@gmail.com>
2024-05-06 13:03:36 +0530

    verify a signature

--- src/secp256k1/signature.rs
+++ src/secp256k1/signature.rs
@@ -7,6 +7,20 @@ pub struct Signature {
     s: UBig,
 }
 
+impl Signature {
+    pub fn new(r: UBig, s: UBig) -> Self {
+        Self { r, s }
+    }
+
+    pub fn r(&self) -> UBig {
+        self.r.clone()
+    }
+
+    pub fn s(&self) -> UBig {
+        self.s.clone()
+    }
+}
+
 impl Display for Signature {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "(r: {:X}, s: {:X})", self.r, self.s)
